f7f6190228ffadff1519892c26e38837bfcd1b8d
Elnakib <kareemahmedelnakib04@gmail.com>
2024-10-02 03:06:30 +0300

    update

---
+++ classesandobjects.py
@@ -0,0 +1,23 @@
+# Objects are an encapsulation of variables and functions into a single entity. 
+# Objects get their variables and functions from classes. 
+# Classes are essentially a template to create your objects
+
+
+class Myclass :
+  variable = "random"
+
+def function(self):
+  print("This is a message inside the class.")
+
+
+# to assign the above class to an object you would do the following:
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+# Now the variable "myobjectx" holds an object of 
+# the class "MyClass" that contains the variable and the function defined within the class called "MyClass".
+
