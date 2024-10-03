020bfbba4c68d2a70ee5fc90efaf195d840c4126
Elnakib <kareemahmedelnakib04@gmail.com>
2024-10-03 23:36:56 +0300

    this file is only meant for testing random codes

--- test.py
+++ test.py
@@ -1,36 +1,5 @@
-'''
-usernames = []
-
-def userInput():
-  global usernames
-  print("Welcome to this testing file\n")
-  username = input("Please type your name:")
-
-  usernames.append(username)
+username, email =input().split()
+password, code = input().split()
 
 
-userInput()
-print("listed usernames are", usernames)
-
-'''
-
-usernames = []
-
-def userInput():
-    while True:
-        print("Welcome to this testing file\n")
-        username = input("Please type your name (or type 'q' to quit): ")
-        
-        if username.lower() == 'q':  # Exit condition
-            break
-        
-        usernames.append(username)  # Add the input to the list
-
-    print("\nList of usernames entered:")
-    for name in usernames:
-        print(name)
-        
-
-userInput()
-
-
+print(username, email, password, code)
