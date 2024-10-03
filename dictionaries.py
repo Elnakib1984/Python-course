2a31db0e8084519395b8894d0ef1050dd63b458b
Elnakib <kareemahmedelnakib04@gmail.com>
2024-10-03 16:11:28 +0300

    update

--- dictionaries.py
+++ dictionaries.py
@@ -26,3 +26,68 @@ print(phonebook)
 phonebook = {"John" : 938477566,"Jack" : 938377264,"Jill" : 947662781}
 for name, number in phonebook.items():
     print("Phone number of %s is %d" % (name, number))
+
+
+# Removing a value
+# To remove a specified index, use either one of the following notations:
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+del phonebook["John"]
+print(phonebook)
+
+# OR : 
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+phonebook.pop("John")
+print(phonebook)
+
+
+
+
+
+
+
+
+
+# Exercise :
+
+phonebook = {  
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}  
+# your code goes here
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook.")
+
+
+
+
+# Solution 
+phonebook = {  
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}  
+
+# your code goes here
+phonebook["Jake"] = 938273443  
+del phonebook["Jill"]  
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook.")
